fb31e9753af43328aca75eb70151a6c1a87d47e0
Nathaniel Jonathan Rusli <omgitsnathaniels@gmail.com>
2026-05-19 14:14:05 +0700

    chore: add api setup

---
+++ src/server/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from db.database import init_db
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Make sure database is initialized before server starts."""
+    print("Starting server and preparing database")
+    init_db()
+    yield
+    print("Stopping server")
+
+app = FastAPI(
+    title="Distributed Password Manager API", 
+    lifespan=lifespan
+)
+
+@app.get("/")
+def validate_at_root():
+    return {
+        "status": "ok", 
+        "message": "Distributed Password Manager API"
+    }
